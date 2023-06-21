0b7795bf3ab3d56da863cd796742abd10481c5f9
nahid12 <nahidraj.web@gmail.com>
2023-06-21 13:24:34 +0600

    responsive done

--- js/script.js
+++ js/script.js
@@ -181,6 +181,35 @@ $(function () {
   });
 
 
+   // mobile menu js
+   $('.mobile-topbar .icon i').click(function () {
+     $('.mobile-menu-main').addClass('show-mobile-menu')
+   })
+   $('.close-m-menu').click(function () {
+     $('.mobile-menu-main').removeClass('show-mobile-menu')
+   })
+   $('.mobile-topbar .icon i').click(function () {
+     $('.mobile-menu-overlay').addClass('show-mobile-menu-overlay')
+   })
+   $('.mobile-topbar .icon i').click(function () {
+     $('.mobile-menu-overlay').addClass('show-mobile-menu-overlay')
+   })
+   $('.close-m-menu').click(function () {
+     $('.mobile-menu-overlay').removeClass('show-mobile-menu-overlay')
+   })
+   $('.mobile-menu-overlay').click(function () {
+     $('.mobile-menu-overlay').removeClass('show-mobile-menu-overlay')
+   })
+   $('.mobile-menu-overlay').click(function () {
+     $('.mobile-menu-main').removeClass('show-mobile-menu')
+   })
+
+   $('.sub-menu ul').hide();
+   $(".sub-menu a").click(function () {
+     $(this).parent(".sub-menu").children("ul").slideToggle("100");
+     $(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
+   });
+
 
 
 
